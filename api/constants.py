840677113b03a2aa4b7b74497d58ae8ad7d0ccc0
NGIPM - Soumya Ranjan Sahoo <SoumyaRanjan.Sahoo@carrier.com>
2021-12-21 22:38:00 +0530

    Added Zappa settings

--- api/constants.py
+++ api/constants.py
@@ -1,14 +1,14 @@
 import os
 
-# DB_ENDPOINT = os.environ['db_endpoint']
-# DB_USER_NAME = os.environ['db_user_name']
-# DB_SECRET_KEY = os.environ['db_password']
-# DATABASE_NAME = os.environ['db_name']
+# DB_ENDPOINT = os.environ['db_endpoint']   # Uncomment while deploying into AWS through zappa
+# DB_USER_NAME = os.environ['db_user_name'] # Uncomment while deploying into AWS through zappa
+# DB_SECRET_KEY = os.environ['db_password'] # Uncomment while deploying into AWS through zappa
+# DATABASE_NAME = os.environ['db_name']     # Uncomment while deploying into AWS through zappa
 
-DB_ENDPOINT = 'localhost'
-DB_USER_NAME = 'root'
-DB_SECRET_KEY = '123456'
-DATABASE_NAME = 'medium_db'
+DB_ENDPOINT = 'localhost'   # Comment while deploying into AWS through zappa
+DB_USER_NAME = 'root'       # Comment while deploying into AWS through zappa
+DB_SECRET_KEY = '123456'    # Comment while deploying into AWS through zappa
+DATABASE_NAME = 'medium_db' # Comment while deploying into AWS through zappa
 
 DB_CONNECTION_STR="mysql+pymysql://"+DB_USER_NAME+":"+DB_SECRET_KEY+"@"+DB_ENDPOINT+"/"+DATABASE_NAME
 
